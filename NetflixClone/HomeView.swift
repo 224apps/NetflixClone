5cc01fa6abcced6ee7cfaf6637ab093563e7f9f9
224apps <224apps@gmail.com>
2022-12-27 03:41:06 -0500

    Home view creation

---
+++ NetflixClone/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NetflixClone
+//
+//  Created by Abdoulaye Diallo on 12/27/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Netflix Clone")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
