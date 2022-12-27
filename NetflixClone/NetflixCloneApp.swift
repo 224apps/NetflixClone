f9dcd478026a096f1520b8f2aaf307ff439a252e
224apps <224apps@gmail.com>
2022-12-27 02:25:40 -0500

    Create Initial SwiftUI Project

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Abdoulaye Diallo on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
